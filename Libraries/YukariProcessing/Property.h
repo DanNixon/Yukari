c9edac921ca31e65af653c213d0c60f4b91262b6
Dan Nixon <dan@dan-nixon.com>
2017-02-23 17:27:25 +0000

    Start property tests

--- Libraries/YukariProcessing/Property.h
+++ Libraries/YukariProcessing/Property.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <functional>
+#include <iostream>
 #include <memory>
 #include <vector>
 
@@ -19,22 +20,64 @@ namespace Processing
     typedef std::function<bool(const ValueStorageType &)> Validator;
 
   public:
-    Property(size_t size = 1);
-    virtual ~Property();
+    Property(size_t size = 1)
+    {
+      m_values.resize(size);
+    }
+
+    Property(const ValueStorageType &values)
+        : m_values(values)
+    {
+    }
+
+    virtual ~Property()
+    {
+      m_values.clear();
+    }
 
     inline size_t size() const
     {
       return m_values.size();
     }
 
-    void setValidator(Validator validator);
-    void removeValidator();
-    bool validate() const;
+    void setValidator(Validator validator)
+    {
+      m_validator = validator;
+    }
 
-    boost::any value() const;
+    void removeValidator()
+    {
+      m_validator = Validator();
+    }
 
-    boost::any operator[](size_t idx) const;
-    boost::any &operator[](size_t idx);
+    bool validate() const
+    {
+      return (m_validator ? m_validator(m_values) : true);
+    }
+
+    template <typename T> T value(size_t idx = 0) const
+    {
+      if (idx >= m_values.size())
+        throw std::runtime_error("Property index out of range");
+
+      return boost::any_cast<T>(m_values[idx]);
+    }
+
+    boost::any operator[](size_t idx) const
+    {
+      if (idx >= m_values.size())
+        throw std::runtime_error("Property index out of range");
+
+      return m_values[idx];
+    }
+
+    boost::any &operator[](size_t idx)
+    {
+      if (idx >= m_values.size())
+        throw std::runtime_error("Property index out of range");
+
+      return m_values[idx];
+    }
 
     inline ValueStorageType::iterator begin()
     {
@@ -56,13 +99,17 @@ namespace Processing
       return m_values.cend();
     }
 
-    friend std::ostream &operator<<(std::ostream &s, Property &o);
-
   protected:
     ValueStorageType m_values;
     Validator m_validator;
   };
 
   typedef std::shared_ptr<Property> Property_sptr;
+
+  std::ostream &operator<<(std::ostream &s, Property &o)
+  {
+    s << "Property[size=" << o.size() << "]";
+    return s;
+  }
 }
 }
--- Libraries/YukariProcessing/test/PropertyTest.cpp
+++ Libraries/YukariProcessing/test/PropertyTest.cpp
@@ -25,7 +25,27 @@ namespace Processing
 
 	  BOOST_CHECK_EQUAL(p.size(), 1);
 	  BOOST_CHECK_EQUAL(str.str(), "Property[size=1]");
+	  BOOST_CHECK(p.validate());
     }
+
+	BOOST_AUTO_TEST_CASE(Property_Init_Multiple_Values)
+	{
+		Property p({21, 24, 29});
+
+		std::stringstream str;
+		str << p;
+
+		BOOST_CHECK_EQUAL(p.size(), 3);
+		BOOST_CHECK_EQUAL(str.str(), "Property[size=3]");
+		BOOST_CHECK(p.validate());
+
+		BOOST_CHECK_EQUAL(p.value<int>(), 21);
+		BOOST_CHECK_EQUAL(p.value<int>(0), 21);
+		BOOST_CHECK_EQUAL(p.value<int>(1), 24);
+		BOOST_CHECK_EQUAL(p.value<int>(2), 29);
+	}
+
+	// TODO
   }
 }
 }
