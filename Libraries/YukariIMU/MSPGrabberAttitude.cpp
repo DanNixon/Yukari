40444556e7b9bec52d865e511454ab53e6cbded8
Dan Nixon <dan@dan-nixon.com>
2017-04-07 11:35:45 +0100

    fix MSP FC IMU

--- Libraries/YukariIMU/MSPGrabberAttitude.cpp
+++ Libraries/YukariIMU/MSPGrabberAttitude.cpp
@@ -51,9 +51,9 @@ namespace IMU
 
     /* Set orientation from attitude data */
     Quaternion x, y, z;
-    rotate_x(x, -m_attitude[2] * DEG_TO_RAD);
+    rotate_x(x, -m_attitude[1] * DEG_TO_RAD);
     rotate_y(y, -m_attitude[0] * DEG_TO_RAD);
-    rotate_z(z, -m_attitude[1] * DEG_TO_RAD);
+    rotate_z(z, -m_attitude[2] * DEG_TO_RAD);
     retVal->orientation() = x * y * z;
 
     return retVal;
