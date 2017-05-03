4ba9adc5f5afb00ff3c9aebe29d73a7eebe2220d
Dan Nixon <dan@dan-nixon.com>
2017-05-03 17:04:52 +0100

    Accel calib on Teeny IMU

--- imu/Teensy/src/imu_device.cpp
+++ imu/Teensy/src/imu_device.cpp
@@ -33,6 +33,7 @@ const float ACCEL_COEFF = 9.81f / 4096.0f;
 
 Quaternion g_quat;
 VectorFloat g_gravity;
+VectorInt16 g_accelCalib;
 VectorInt16 g_accel;
 VectorInt16 g_realAccel;
 VectorInt16 g_worldAccel;
@@ -95,6 +96,9 @@ void taskDMP()
 
     g_imu.dmpGetQuaternion(&g_quat, g_dmpFIFOBuffer);
     g_imu.dmpGetAccel(&g_accel, g_dmpFIFOBuffer);
+    g_accel.x -= g_accelCalib.x;
+    g_accel.y -= g_accelCalib.y;
+    g_accel.z -= g_accelCalib.z;
     g_imu.dmpGetGravity(&g_gravity, &g_quat);
     g_imu.dmpGetLinearAccel(&g_realAccel, &g_accel, &g_gravity);
     g_imu.dmpGetLinearAccelInWorld(&g_worldAccel, &g_realAccel, &g_quat);
@@ -156,7 +160,6 @@ void taskBarometer()
 void taskPrintData()
 {
   // Device orientation as quaternion
-  /*
   DEBUG_SERIAL.printf("quat\t");
   DEBUG_SERIAL.print(g_quat.w);
   DEBUG_SERIAL.print("\t");
@@ -165,16 +168,15 @@ void taskPrintData()
   DEBUG_SERIAL.print(g_quat.y);
   DEBUG_SERIAL.print("\t");
   DEBUG_SERIAL.println(g_quat.z);
-  */
 
   // Linear acceleration
-  // DEBUG_SERIAL.printf("a\t%d\t%d\t%d\n", g_accel.x, g_accel.y, g_accel.z);
+  DEBUG_SERIAL.printf("a\t%d\t%d\t%d\n", g_accel.x, g_accel.y, g_accel.z);
 
   // Linear acceleration without gravity
-  // DEBUG_SERIAL.printf("aReal\t%d\t%d\t%d\n", g_realAccel.x, g_realAccel.y, g_realAccel.z);
+  DEBUG_SERIAL.printf("aReal\t%d\t%d\t%d\n", g_realAccel.x, g_realAccel.y, g_realAccel.z);
 
   // Linear acceleration without gravity and corrected for orientation
-  // DEBUG_SERIAL.printf("aWorld\t%d\t%d\t%d\n", g_worldAccel.x, g_worldAccel.y, g_worldAccel.z);
+  DEBUG_SERIAL.printf("aWorld\t%d\t%d\t%d\n", g_worldAccel.x, g_worldAccel.y, g_worldAccel.z);
 
   // Linear acceleration without gravity and corrected for orientation in ms-2
   DEBUG_SERIAL.print("aWorld (ms-2)\t");
@@ -209,16 +211,69 @@ void taskPrintData()
   DEBUG_SERIAL.println(g_displacement.z);
 
   // BMP180 data
-  /*
   DEBUG_SERIAL.printf("T/P/Alt\t");
   DEBUG_SERIAL.print(g_temperature);
   DEBUG_SERIAL.print("\t");
   DEBUG_SERIAL.print(g_pressure);
   DEBUG_SERIAL.print("\t");
   DEBUG_SERIAL.println(g_altitude);
-  */
 }
 #endif /* DEBUG */
+
+#ifdef SERIALPLOT
+/*
+ * Prints debug data as CSV for serialplot.
+ *
+ *  1) Quaternion - w
+ *  2) Quaternion - x
+ *  3) Quaternion - y
+ *  4) Quaternion - z
+ *  5) Acceleration - x
+ *  6) Acceleration - y
+ *  7) Acceleration - z
+ *  8) Linear acceleration - x
+ *  9) Linear acceleration - y
+ * 10) Linear acceleration - z
+ * 11) World acceleration - x
+ * 12) World acceleration - y
+ * 13) World acceleration - z
+ * 14) Velocity - x
+ * 15) Velocity - y
+ * 16) Velocity - z
+ * 17) Displacement - x
+ * 18) Displacement - y
+ * 19) Displacement - z
+ */
+void taskDebugPrintCSV()
+{
+  DEBUG_SERIAL.print(g_quat.w);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_quat.x);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_quat.y);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_quat.z);
+  DEBUG_SERIAL.print(",");
+
+  DEBUG_SERIAL.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,", g_accel.x, g_accel.y, g_accel.z, g_realAccel.x,
+                      g_realAccel.y, g_realAccel.z, g_worldAccel.x, g_worldAccel.y, g_worldAccel.z);
+
+  DEBUG_SERIAL.print(g_velocity.x);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_velocity.y);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_velocity.z);
+  DEBUG_SERIAL.print(",");
+
+  DEBUG_SERIAL.print(g_displacement.x);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_displacement.y);
+  DEBUG_SERIAL.print(",");
+  DEBUG_SERIAL.print(g_displacement.z);
+
+  DEBUG_SERIAL.println();
+}
+#endif /* SERIALPLOT */
 
 void taskFeedGPS()
 {
@@ -264,13 +319,13 @@ void imu_device_init()
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
 
-#if defined(DEBUG)
+#if defined(DEBUG) || defined(SERIALPLOT)
   /* Init debug serial */
   DEBUG_SERIAL.begin(DEBUG_BAUD);
   while (!DEBUG_SERIAL)
     delay(5);
   DEBUG_SERIAL.printf("Serial up\n");
-#endif /* DEBUG */
+#endif /* DEBUG || SERIALPLOT */
 
   /* Init MSP */
   MSP_SERIAL.begin(MSP_BAUD);
@@ -346,12 +401,38 @@ void imu_device_init()
 #ifndef DISABLE_IMU
   /* Init IMU */
   g_imu.initialize();
-#ifdef DEBUG
-  DEBUG_SERIAL.printf("IMU init: %d\n", g_imu.testConnection());
+#ifdef DEBU2
+  DEBUG_SERIuAL.printf("IMU init: %d\n", g_imu.testConnection());
+#endif /* DEBUG */
+
+  /* Calibration sample */
+  int64_t axa = 0;
+  int64_t aya = 0;
+  int64_t aza = 0;
+  const uint16_t numSamples = 1000;
+  int16_t dummy, ax, ay, az;
+#ifdef DEBU2
+  DEBUG_SERIuAL.printf("Accel. calibration start\n");
+#endif /* DEBUG */
+  for (uint16_t i = 0; i < numSamples; i++)
+  {
+    g_imu.getMotion6(&ax, &ay, &az, &dummy, &dummy, &dummy);
+    axa += ax;
+    aya += ay;
+    aza += az;
+    delay(1);
+  }
+  g_accelCalib.x = axa / numSamples;
+  g_accelCalib.y = aya / numSamples;
+  g_accelCalib.z = aza / numSamples;
+#ifdef DEBU2
+  DEBUG_SERIuAL.printf("Accel. calibration done: %d, %d, %d\n", g_accelCalib.x, g_accelCalib.y,
+                       g_accelCalib.z);
 #endif /* DEBUG */
 
   pinMode(MPU_INTERRUPT_PIN, INPUT);
 
+  /* Init DMP */
   uint8_t dmpStatus = g_imu.dmpInitialize();
   if (dmpStatus == 0)
   {
@@ -376,6 +457,9 @@ void imu_device_init()
   g_scheduler.addTask(&taskBlink, Scheduler::HzToUsInterval(5.0f));
   g_scheduler.addTask(&taskMSP, 0);
   g_scheduler.addTask(&taskResetIMUIntegration, Scheduler::HzToUsInterval(10.0f));
+#ifdef SERIALPLOT
+  g_scheduler.addTask(&taskDebugPrintCSV, Scheduler::HzToUsInterval(10.0f));
+#endif /* SERIALPLOT */
 #ifndef DISABLE_GPS
   g_scheduler.addTask(&taskFeedGPS, 0);
 #endif /* DISABLE_GPS */
